fef1a769cc882b0934d677a1e551db00324cdffe
Fang-Ting Kao <fkao@e1z2r7p4.42.us.org>
2017-05-22 16:02:40 -0700

    commit ftpf/libft

---
+++ libft/ft_itoa.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkao <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/06 14:01:33 by fkao              #+#    #+#             */
+/*   Updated: 2017/03/06 19:21:30 by fkao             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char			*ft_itoa(int n)
+{
+	char		*str;
+	char		*ptr;
+	size_t		len;
+	long		nbr;
+
+	nbr = (long)n;
+	len = ft_countint(nbr);
+	str = ft_strnew(len);
+	if (str)
+	{
+		ptr = str + len - 1;
+		if (nbr < 0)
+		{
+			*str = '-';
+			nbr *= -1;
+		}
+		while (nbr > 9)
+		{
+			*ptr-- = '0' + (nbr % 10);
+			nbr /= 10;
+		}
+		*ptr = '0' + nbr;
+	}
+	return (str);
+}
