546bab6c5c105995b5428b184bfe376451411bf1
Fang-Ting Kao <fkao@e1z2r8p27.42.us.org>
2017-06-21 17:33:59 -0700

    test files

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fkao <fkao@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/22 19:43:57 by fkao              #+#    #+#             */
-/*   Updated: 2017/06/19 15:36:58 by fkao             ###   ########.fr       */
+/*   Updated: 2017/06/21 17:32:25 by fkao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,9 @@ int		ft_printf(const char *format, ...);
 int		main(void)
 {
 	setlocale(LC_ALL, "");
-	printf("%x\n", LONG_MIN - 1);
-	printf("%C\n", L'猫');
 	printf("%s\n", "printf:");
-	printf("\treturn: %d\n", printf("%15.4S", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B"));
+	printf("\treturn: %d\n", printf("%15.4S", L"ĦƐȽĿO™ ŴØɌ ŁƉ"));
 	ft_printf("%s\n", "ft_printf:");
-	ft_printf("\treturn: %d\n", ft_printf("%15.4S", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B"));
+	ft_printf("\treturn: %d\n", ft_printf("%15.4S", L"ĦƐȽĿO™ ŴØɌ ŁƉ"));
 	return (0);
 }
